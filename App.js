9945dce3361ddb642ac5c7150bbb3bf5e1224b34
Mahdi <mathfractal@yahoo.com>
2021-09-27 23:47:57 +0330

    commit

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.shape_container}>
+          <View style={styles.square} />
+        </View>
+        <View style={styles.shape_container}>
+          <View style={styles.rectangle} />
+        </View>
+        <View style={styles.shape_container}>
+          <View style={styles.circle} />
+        </View>
+        <View style={styles.shape_container}>
+          <View style={styles.oval} />
+        </View>
+        <View style={styles.shape_container}>
+          <View style={styles.triangle} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  shape_container: {
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  square: {
+    width: 120,
+    height: 120,
+    backgroundColor: '#264653',
+  },
+  rectangle: {
+    width: 120 * 2,
+    height: 120,
+    backgroundColor: '#2a9d8f'
+  },
+  circle: {
+    width: 120,
+    height: 120,
+    borderRadius: 120 / 2,
+    borderColor: '#e9c46a',
+    borderWidth: 5,
+  },
+  oval: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    backgroundColor: '#f4a261',
+    transform: [{ scaleX: 2 }],
+  },
+  triangle: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftWidth: 60,
+    borderRightWidth: 60,
+    borderBottomWidth: 120,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#e76f51',
+    transform: [{ rotate: '180deg' }]
+  }
 });
